d9ca9b1ff4ad6ec45e2b6304471c359c1aec791e
Krushna <krushnatale187@gmail.com>
2023-04-05 15:56:48 +0530

    First commit

---
+++ randamnumbers.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class randamnumbers {
+   public static void main(String[] args) {
+      Random rand = new Random();
+
+      for (int i = 0; i < 10; i++) {
+         int randomNum = rand.nextInt(100);
+         System.out.println("Random Number " + (i + 1) + ": " + randomNum);
+      }
+   }
+}
